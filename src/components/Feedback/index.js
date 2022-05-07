f9967534b111b17dfeb1f366ec118861c51e50f9
prashanth-sakarla <sakarlaprashanth@gmail.com>
2022-05-07 07:30:38 +0000

    codding-11

---
+++ src/components/Feedback/index.js
@@ -0,0 +1,12 @@
+// Write your React code here.
+import {Component} from 'react'
+
+import './index.css'
+
+class Feedback extends Component {
+  render() {
+    return <div className="bg-container"></div>
+  }
+}
+
+export default Feedback
